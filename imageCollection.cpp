6ba2cc2951cf8fd8711e5c4cb6b75c01e67a8a22
albertJ32 <00b0d14c@opayq.com>
2015-08-05 17:02:52 +0100

    prints current dir contents, stores stream img to Mat on demand

--- imageCollection.cpp
+++ imageCollection.cpp
@@ -1,8 +1,10 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
-#include <stdio.h>
-#include <fstream>
+#include <stdio.h> // for printf()
+#include <stdlib.h> // for exit() function
+#include <fstream> // For videoStream
+#include <dirent.h> // For accessing filesystem
 
 using namespace std;
 using namespace cv;
@@ -17,6 +19,29 @@ void menuPrint(){
 
 int main(){
 
+  DIR *pdir = NULL;
+
+  pdir = opendir("../");
+  // Check that dir was initialised correctly
+  if(pdir == NULL){
+    cout << "ERROR! unable to open directory, exiting." << endl;
+    exit(1);
+  }
+
+  struct dirent *pent = NULL;
+
+  // Continue as long as there are still values in the dir list
+  while (pent = readdir(pdir)){
+    if(pdir==NULL){
+      cout << "ERROR! dir was not initialised correctly, exiting." << endl;
+      exit(3);
+    }
+    cout << pent->d_name << endl;
+  }
+
+  closedir(pdir);
+  cout << "finished Reading Successfully.." << endl;
+
   VideoCapture stream(0);
   if(!stream.isOpened()){
     cout << "Video stream unable to be opened exiting.." << endl;
@@ -26,22 +51,26 @@ int main(){
 
   menuPrint();
 
+  namedWindow("savedImage", CV_WINDOW_AUTOSIZE);
+  Mat savedImage;
+  savedImage =  Mat::zeros(640,480,CV_8UC3);
+
   while(true){
     Mat inputTmp;
     stream.read(inputTmp);
     imshow("VideoStream", inputTmp);
     char c = waitKey(30);
 
-    if(c == 'q'){
+    if(c == 't'){
+      cout << "capturing textons images" << endl;
+      savedImage = inputTmp.clone();
+      imshow("savedImage", savedImage);
+      menuPrint();
+    }else if(c == 'q'){
       cout << "quitting.. " << endl;
       break;
-    }else if(c == 't'){
-      cout << "capturing textons images" << endl;
-      menuPrint();
     }
-
   }
 
-
-  return 1;
+  return 0;
 }
